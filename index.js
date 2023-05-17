e7fd4d8dc5520f1b6e0b9d4738bb5a5d587da618
Ricky TI <tobaracing1@gmail.com>
2023-05-18 00:01:58 +0700

    Add user route to app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
+const usersRouter = require("./routes/users")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use('/users', usersRouter)
 const port = process.env.PORT || 8080
 
 
@@ -12,5 +14,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("App listening on port ${port}")
+    console.log(`App listening on port ${port}`)
 })
