21ebff7a25bdeb3ea23647a8eb61cf11384cf1cf
Pikacat <faisalthariq894@gmail.com>
2023-05-23 18:49:13 +0700

    update

--- routes/response.js
+++ routes/response.js
@@ -5,16 +5,26 @@ const { DB } = require('../config')
 //router
 router.get('/response', async (req, res) => {
 try {
+    const dateTime = new Date(currect);
+    const date = dateTime.getDate() + "-" + dateTime.getMonth() + "-" + dateTime.getFullYear();
+    const responseID =  crypto.randomUUID() + date + dateTime.getMilliseconds();
 
 } catch (error) {
     
 }
+})
 
-router.get('/response/:responseID')
+router.get('/response/:responseID', async(req, res) =>{
 try {
     const sID = req.body.surveyID;
     const uID = req.body.uid;
     const aID = req.body.answer;
+
+    surveyresponse = {
+        "surveyID" : sID,
+        "uid" : uID,
+        "answer" : aID
+    }
 } catch (error) {
     
 }
