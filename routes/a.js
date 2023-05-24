5f7f800d3163e1fe584cfe053ec982bd945f4322
Ricky TI <tobaracing1@gmail.com>
2023-05-24 20:48:22 +0700

    Temp

---
+++ routes/a.js
@@ -0,0 +1,9 @@
+
+
+
+const a =
+{
+    status : "Failed",
+    code : "surveys/notFound",
+    "Message" : "Survey does not exist" 
+}
--- routes/response.js
+++ routes/response.js
@@ -1,22 +1,44 @@
 const express = require("express")
 const router  = express.Router()
+const { DB } = require('../config')
 const crypto = require('crypto')
+const { verifyToken } = require("../middleware/verifyToken")
 
 //router
-router.post('/response', async (req, res) => {
-try {
+router.post('/create', async (req, res) => {
+    
+    try {
+        const {answers, surveyID, reward} = req.body;
+        const uid = req.body.uid;
 
-    const dateTime = new Date(currect);
-    const date = dateTime.getDate() + "-" + dateTime.getMonth() + "-" + dateTime.getFullYear();
-    const responseID =  crypto.randomUUID() + date + dateTime.getMilliseconds();
-    res.status(200).json(response)
+        const current = Date.now()
+        const dateTime = new Date(current);
+        const date = dateTime.getDate() + "-" + dateTime.getMonth() + "-" + dateTime.getFullYear();
+        const responseID =  crypto.randomUUID() + date + dateTime.getMilliseconds();
 
-} catch (error) {
-    console.error(error)
-    res.status(500).json({
-        message : error
-    })
-}
+        const surveyRef = DB.collection('surveys').doc(req.body.surveyID)
+        const docSnapshot = await surveyRef.get() 
+
+        if(!docSnapshot.exist){
+            return res.status(404).json({
+                status : "Failed",
+                code : 'surveys/notFound',
+                message : "Survey not found"
+            })
+        }
+
+        const surveyQuota = docSnapshot.data().quota
+
+
+
+        res.status(200).json(surveyQuota)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message : error
+        })
+    }
 })
 
 router.get('/response/:responseID', async(req, res) =>{
@@ -39,3 +61,5 @@ try {
     })
 }
 })
+
+module.exports = router
