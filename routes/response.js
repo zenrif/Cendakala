ed5bc3bf4d9036200350d9398f7a4882fdd27a71
Pikacat <faisalthariq894@gmail.com>
2023-05-24 15:29:55 +0700

    update response.js

--- routes/response.js
+++ routes/response.js
@@ -1,31 +1,38 @@
 const express = require("express")
 const router  = express.Router()
-const { DB } = require('../config')
 
 //router
-router.get('/response', async (req, res) => {
+router.post('/response', async (req, res) => {
 try {
+
     const dateTime = new Date(currect);
     const date = dateTime.getDate() + "-" + dateTime.getMonth() + "-" + dateTime.getFullYear();
     const responseID =  crypto.randomUUID() + date + dateTime.getMilliseconds();
+    res.status(200).json(response)
 
 } catch (error) {
-    
+    console.error(error)
+    res.status(500).json({
+        message : error
+    })
 }
 })
 
 router.get('/response/:responseID', async(req, res) =>{
 try {
+
     const sID = req.body.surveyID;
     const uID = req.body.uid;
     const aID = req.body.answer;
 
-    surveyresponse = {
+    const surveyresponse = {
         "surveyID" : sID,
         "uid" : uID,
         "answer" : aID
     }
-} catch (error) {
     
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
 }
 })
