f572d0eccac12616a0ecae05f15129acb1277925
Pikacat <faisalthariq894@gmail.com>
2023-05-24 16:05:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const config = require('./config')
 const usersRouter = require("./routes/users")
 const surveysRouter = require("./routes/surveys")
-const surveysRouter = require("./routes/response")
+const responseRouter = require("./routes/response")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/users', usersRouter)
 app.use('/surveys', surveysRouter)
-app.use('/response', surveysRouter)
+app.use('/response', responseRouter)
 const port = process.env.PORT || 8080
 
 
